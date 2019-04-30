b43f5e2aaa64574a15b9adbf3326013730be5515
Tim Field <tfield@salmon.com>
2019-04-29 18:35:04 -0700

    Add Event List and Event Summary API

--- event_api_test.go
+++ event_api_test.go
@@ -1,6 +1,7 @@
 package intercom
 
 import (
+	"io/ioutil"
 	"testing"
 	"time"
 
@@ -24,9 +25,64 @@ func TestEventAPISaveFail(t *testing.T) {
 	}
 }
 
+func TestEventAPIList(t *testing.T) {
+	http := TestEventHTTPClient{t: t, expectedURI: "/events", fixtureFilename: "fixtures/event_list.json"}
+	api := EventAPI{httpClient: &http}
+	params := eventParams{UserID: "444"}
+	eventList, _ := api.list(params)
+	if len(eventList.Events) != 2 {
+		t.Errorf("Error retrieving event list")
+	}
+	if eventList.Events[0].ID != "1234" {
+		t.Errorf("Incorrect EventID")
+	}
+	if eventList.Events[1].ID != "5678" {
+		t.Errorf("Incorrect EventID")
+	}
+}
+
+func TestEventAPIListFail(t *testing.T) {
+	http := TestEventHTTPClient{t: t, expectedURI: "/events", shouldFail: true}
+	api := EventAPI{httpClient: &http}
+	params := eventParams{UserID: "444"}
+	_, err := api.list(params)
+	if herr, ok := err.(interfaces.HTTPError); ok && herr.Code != "not_found" {
+		t.Errorf("Error not returned")
+	}
+}
+
+func TestEventAPISummary(t *testing.T) {
+	http := TestEventHTTPClient{t: t, expectedURI: "/events", fixtureFilename: "fixtures/event_summaries.json"}
+	api := EventAPI{httpClient: &http}
+	params := eventParams{UserID: "444"}
+	eventList, _ := api.summary(params)
+	if len(eventList.Events) != 2 {
+		t.Errorf("Error retrieving event list")
+	}
+	if eventList.Events[0].Count != 1234 {
+		t.Errorf("Incorrect Count")
+	}
+	if eventList.Events[1].Count != 5678 {
+		t.Errorf("Incorrect Count")
+	}
+}
+
+func TestEventAPISummaryFail(t *testing.T) {
+	http := TestEventHTTPClient{t: t, expectedURI: "/events", shouldFail: true}
+	api := EventAPI{httpClient: &http}
+	params := eventParams{UserID: "444"}
+	_, err := api.summary(params)
+	if herr, ok := err.(interfaces.HTTPError); ok && herr.Code != "not_found" {
+		t.Errorf("Error not returned")
+	}
+}
+
 type TestEventHTTPClient struct {
 	TestHTTPClient
-	t           *testing.T
+	t *testing.T
+
+	fixtureFilename string
+
 	expectedURI string
 	shouldFail  bool
 }
@@ -40,4 +96,15 @@ func (t TestEventHTTPClient) Post(uri string, event interface{}) ([]byte, error)
 		return nil, err
 	}
 	return nil, nil
+}
+
+func (t TestEventHTTPClient) Get(uri string, event interface{}) ([]byte, error) {
+	if uri != "/events" {
+		t.t.Errorf("Wrong endpoint called")
+	}
+	if t.shouldFail {
+		err := interfaces.HTTPError{StatusCode: 404, Code: "not_found", Message: "User Not Found"}
+		return nil, err
+	}
+	return ioutil.ReadFile(t.fixtureFilename)
 }
--- event_test.go
+++ event_test.go
@@ -41,11 +41,76 @@ func successBody(t *testing.T, event Event) error {
 	return nil
 }
 
+func TestEventList(t *testing.T) {
+	eventList, _ := (&EventService{Repository: TestEventAPI{t: t, eventList: eventListSuccess}}).ListByUserID("1")
+	if eventList.Events[0].UserID != "1" {
+		t.Errorf("no events for user id found")
+	}
+	eventList, _ = (&EventService{Repository: TestEventAPI{t: t, eventList: eventListSuccess}}).ListByEmail("2")
+	if eventList.Events[0].Email != "2" {
+		t.Errorf("no events for email found")
+	}
+	eventList, _ = (&EventService{Repository: TestEventAPI{t: t, eventList: eventListSuccess}}).ListByID("3")
+	if eventList.Events[0].IntercomUserID != "3" {
+		t.Errorf("no events for Intercom user id found")
+	}
+
+	_, err := (&EventService{Repository: TestEventAPI{t: t, eventList: eventListFail}}).ListByID("3")
+	if err.Error() != "event list fail" {
+		t.Errorf("error not propagated")
+	}
+}
+
+func eventListSuccess(params eventParams) (EventList, error) {
+	return EventList{Events: []Event{{UserID: params.UserID, IntercomUserID: params.IntercomUserID, Email: params.Email}}}, nil
+}
+
+func eventListFail(_ eventParams) (EventList, error) {
+	return EventList{}, errors.New("event list fail")
+}
+
+func TestEventSummary(t *testing.T) {
+	eventSummary, _ := (&EventService{Repository: TestEventAPI{t: t, eventSummary: eventSummarySuccess}}).SummaryByUserID("1")
+	if eventSummary.UserID != "1" {
+		t.Errorf("no events for user id found")
+	}
+	eventSummary, _ = (&EventService{Repository: TestEventAPI{t: t, eventSummary: eventSummarySuccess}}).SummaryByEmail("2")
+	if eventSummary.Email != "2" {
+		t.Errorf("no events for email found")
+	}
+	eventSummary, _ = (&EventService{Repository: TestEventAPI{t: t, eventSummary: eventSummarySuccess}}).SummaryByID("3")
+	if eventSummary.IntercomUserID != "3" {
+		t.Errorf("no events for Intercom user id found")
+	}
+
+	_, err := (&EventService{Repository: TestEventAPI{t: t, eventSummary: eventSummaryFail}}).SummaryByID("3")
+	if err.Error() != "event summary fail" {
+		t.Errorf("error not propagated")
+	}
+}
+
+func eventSummarySuccess(params eventParams) (EventSummaries, error) {
+	return EventSummaries{UserID: params.UserID, IntercomUserID: params.IntercomUserID, Email: params.Email}, nil
+}
+
+func eventSummaryFail(_ eventParams) (EventSummaries, error) {
+	return EventSummaries{}, errors.New("event summary fail")
+}
+
 type TestEventAPI struct {
-	t    *testing.T
-	body func(*testing.T, Event) error
+	t            *testing.T
+	body         func(*testing.T, Event) error
+	eventList    func(eventParams) (EventList, error)
+	eventSummary func(eventParams) (EventSummaries, error)
 }
 
 func (t TestEventAPI) save(event *Event) error {
 	return t.body(t.t, *event)
+}
+
+func (t TestEventAPI) list(params eventParams) (EventList, error) {
+	return t.eventList(params)
+}
+func (t TestEventAPI) summary(params eventParams) (EventSummaries, error) {
+	return t.eventSummary(params)
 }
